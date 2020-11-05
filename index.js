ed00eb0087a7217fd90a0dcf9acf9daa9c0a2f65
jsingle345 <jsingle345@gmail.com>
2020-11-04 19:44:55 -0600

    commit tree 3ec8a7d63ec3606c574a8c87eb21270f85221e01

--- index.js
+++ index.js
@@ -1,1 +1,75 @@
 // Your code here
+
+const mapToNegativize = (array) => {
+    let newArray = []
+
+    for(let i=0; i < array.length; i++){
+        newArray.push(array[i] * (-1))
+    }
+    return newArray
+}
+
+const mapToNoChange = (array) => {
+    let newArray = []
+
+    for(let i=0; i < array.length; i++){
+       newArray.push(array[i])  
+    }
+    return newArray
+}
+
+const mapToDouble = (array) => {
+    let newArray = []
+
+    for(let i=0; i < array.length; i++){
+       newArray.push(array[i] * 2)  
+    }
+    return newArray
+}
+
+const mapToSquare = (array) => {
+    let newArray = []
+
+    for(let i=0; i < array.length; i++){
+       newArray.push(array[i] ** 2)  
+    }
+    return newArray
+}
+
+const reduceToTotal = (array, startingPoint) => {
+    let total 
+
+    if(startingPoint){
+        total = startingPoint
+    } else {
+        total = 0
+    }
+
+    for(const item in array){
+        total += array[item]
+    }
+
+    return total
+}
+
+const reduceToAllTrue = (array) => {
+    let arrayBoolean
+    for(const item in array){
+        if (array[item]){
+        arrayBoolean = true
+        } else {
+        arrayBoolean = false
+        }
+    }
+    return arrayBoolean
+}
+
+const reduceToAnyTrue = (array) => {
+    let arrayBoolean = false
+    for(const item in array){
+        if (array[item]){
+            arrayBoolean = true
+    }
+}
+    return arrayBoolean
+}
